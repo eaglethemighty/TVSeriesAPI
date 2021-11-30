1f84b454b0edfe07dea8ceb37dbb0e6dc505ffa4
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-11-30 10:40:34 +0100

    Generic interface for CRUD operations in repository pattern

--- TVSeriesAPI/TVSeriesAPI/DAL/Repositories/IRepository.cs
+++ TVSeriesAPI/TVSeriesAPI/DAL/Repositories/IRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace TVSeriesAPI.DAL.Repositories
 {
-    public interface IRepository
+    public interface IRepository<TModel>
     {
+        Task<TModel> GetById(int id);
+        Task<ICollection<TModel>> GetAll();
+        Task AddAsync(TModel obj);
+        Task DeleteAsync(TModel obj);
+        Task UpdateAsync(TModel obj);
+        Task<bool> SaveChanges();
     }
 }
