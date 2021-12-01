4cabf7c10172b95ad4c08628f940defb0941409b
LCzunko <LCzunko@gmail.com>
2021-12-01 15:02:54 +0100

    Add custom error class

---
+++ TVSeriesAPI/TVSeriesAPI/Controllers/Errors/BadRequestError.cs
@@ -0,0 +1,17 @@
+﻿namespace TVSeriesAPI.Controllers.Errors
+{
+    public class BadRequestError
+    {
+        public BadRequestError(string traceId, Dictionary<string, string> errors)
+        {
+            this.TraceId = traceId;
+            this.Errors = errors;
+        }
+
+        public string Type { get; set; } = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+        public string Title { get; set; } = "One or more validation errors occurred.";
+        public int Status { get; set; } = 400;
+        public string TraceId { get; set; }
+        public Dictionary<string,string> Errors { get; set; }
+    }
+}
