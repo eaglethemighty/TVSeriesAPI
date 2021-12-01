ce2303601d96c57f06a654430984d2c4bb05a551
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-12-01 01:50:27 +0100

    Develop generic repository interface that allows to join in get operation

---
+++ TVSeriesAPI/TVSeriesAPI/DAL/Extensions/IncludableJoin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace TVSeriesAPI.DAL.Extensions
+{
+    public interface IIncludableJoin<out TEntity, out TProperty> : IQueryable<TEntity>
+    {
+    }
+    public class IncludableJoin<TEntity, TPreviousProperty> : IIncludableJoin<TEntity, TPreviousProperty>
+    {
+        private readonly IIncludableQueryable<TEntity, TPreviousProperty> _query;
+
+        public IncludableJoin(IIncludableQueryable<TEntity, TPreviousProperty> query)
+        {
+            _query = query;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return _query.GetEnumerator();
+        }
+
+        public Expression Expression => _query.Expression;
+        public Type ElementType => _query.ElementType;
+        public IQueryProvider Provider => _query.Provider;
+
+        internal IIncludableQueryable<TEntity, TPreviousProperty> GetQuery()
+        {
+            return _query;
+        }
+    }
+}
---
+++ TVSeriesAPI/TVSeriesAPI/DAL/Repositories/Interfaces/IRepositoryJoin.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using TVSeriesAPI.DAL.Extensions;
+
+namespace TVSeriesAPI.DAL.Repositories.Interfaces
+{
+    public interface IRepository<TModel>
+    {
+        Task<TModel> GetById(int id);
+        Task<ICollection<TModel>> GetAll();
+        Task AddAsync(TModel obj);
+        Task DeleteAsync(TModel obj);
+        Task UpdateAsync(TModel obj);
+        Task<bool> SaveChanges();
+    }
+
+    public interface IRepositoryJoin<TEntity> where TEntity : class
+    {
+        IIncludableJoin<TEntity, TProperty> Join<TProperty>(Expression<Func<TEntity, TProperty>> navigationProperty);
+        Task<TEntity> GetById(int id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity obj);
+        Task DeleteAsync(TEntity obj);
+        Task UpdateAsync(TEntity obj);
+        Task<bool> SaveChanges();
+    }
+}
