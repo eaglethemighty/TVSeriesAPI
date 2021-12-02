6e127b15c249d6fb0e1b4f21d9a89a8b7406a081
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-12-02 11:59:14 +0100

    Complement ThenJoin with one to one relation

--- TVSeriesAPI/TVSeriesAPI/DAL/Extensions/QueryAbleExtensions.cs
+++ TVSeriesAPI/TVSeriesAPI/DAL/Extensions/QueryAbleExtensions.cs
@@ -14,14 +14,14 @@ namespace TVSeriesAPI.DAL.Extensions
             return new IncludableJoin<TEntity, TProperty>(query.Include(propToExpand));
         }
 
-/*        public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
+        public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
              this IIncludableJoin<TEntity, TPreviousProperty> query,
              Expression<Func<TPreviousProperty, TProperty>> propToExpand)
             where TEntity : class
         {
             IIncludableQueryable<TEntity, TPreviousProperty> queryable = ((IncludableJoin<TEntity, TPreviousProperty>)query).GetQuery();
             return new IncludableJoin<TEntity, TProperty>(queryable.ThenInclude(propToExpand));
-        }*/
+        }
 
         public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
             this IIncludableJoin<TEntity, IList<TPreviousProperty>> query,
