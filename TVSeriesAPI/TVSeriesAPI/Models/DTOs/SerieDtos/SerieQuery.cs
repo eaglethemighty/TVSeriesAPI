ce63498c2862ed3708c20405ea611d15aef7f543
LCzunko <LCzunko@gmail.com>
2021-12-02 15:09:58 +0100

    Add endpoint using query string to search/filter series

---
+++ TVSeriesAPI/TVSeriesAPI/Models/DTOs/SerieDtos/SerieQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TVSeriesAPI.Models.DTOs
+{
+    public class SerieQuery
+    {
+        [Range(1, 99999)]
+        public int Page { get; set; } = 1;
+
+        [StringLength(255, MinimumLength = 1)]
+        public string Filter { get; set; } = "*";
+
+        public bool Sort { get; set; } = false;
+    }
+}
