8985014c823bfaee79db400585e15dfaef9bdd30
LCzunko <LCzunko@gmail.com>
2021-12-02 13:24:31 +0100

    Improve auth controller documentation, add annotations to UserCredentials model

--- TVSeriesAPI/TVSeriesAPI/Models/Auth/UserCredentials.cs
+++ TVSeriesAPI/TVSeriesAPI/Models/Auth/UserCredentials.cs
@@ -1,8 +1,15 @@
-﻿namespace TVSeriesAPI.Models.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TVSeriesAPI.Models.Auth
 {
     public class UserCredential
     {
+        [Required]
+        [StringLength(64, MinimumLength = 1)]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(64, MinimumLength = 1)]
         public string Password { get; set; }
     }
 }
