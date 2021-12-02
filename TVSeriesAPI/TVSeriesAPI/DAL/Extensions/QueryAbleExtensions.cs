22f36b4467f1ad9ab63dd331a5dc486ee2bc6271
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-12-02 11:57:55 +0100

    Improve thenjoin exteniosn method

--- TVSeriesAPI/TVSeriesAPI/DAL/Extensions/QueryAbleExtensions.cs
+++ TVSeriesAPI/TVSeriesAPI/DAL/Extensions/QueryAbleExtensions.cs
@@ -14,21 +14,21 @@ namespace TVSeriesAPI.DAL.Extensions
             return new IncludableJoin<TEntity, TProperty>(query.Include(propToExpand));
         }
 
-        public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
+/*        public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
              this IIncludableJoin<TEntity, TPreviousProperty> query,
              Expression<Func<TPreviousProperty, TProperty>> propToExpand)
             where TEntity : class
         {
             IIncludableQueryable<TEntity, TPreviousProperty> queryable = ((IncludableJoin<TEntity, TPreviousProperty>)query).GetQuery();
             return new IncludableJoin<TEntity, TProperty>(queryable.ThenInclude(propToExpand));
-        }
+        }*/
 
         public static IIncludableJoin<TEntity, TProperty> ThenJoin<TEntity, TPreviousProperty, TProperty>(
-            this IIncludableJoin<TEntity, IEnumerable<TPreviousProperty>> query,
+            this IIncludableJoin<TEntity, IList<TPreviousProperty>> query,
             Expression<Func<TPreviousProperty, TProperty>> propToExpand)
             where TEntity : class
         {
-            var queryable = ((IncludableJoin<TEntity, IEnumerable<TPreviousProperty>>)query).GetQuery();
+            var queryable = ((IncludableJoin<TEntity, IList<TPreviousProperty>>)query).GetQuery();
             var include = queryable.ThenInclude(propToExpand);
             return new IncludableJoin<TEntity, TProperty>(include);
         }
